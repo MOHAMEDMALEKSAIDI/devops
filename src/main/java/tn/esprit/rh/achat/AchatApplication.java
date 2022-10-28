68a2b7274346a5aa86d77c9cf36c8990a71526bf
MOHAMEDMALEKSAIDI <74025798+MOHAMEDMALEKSAIDI@users.noreply.github.com>
2022-10-28 13:33:03 +0200

    modif

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -2,10 +2,12 @@ package tn.esprit.rh.achat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan({"tn.esprit.rh.achat.repositories"})
 public class AchatApplication {
 
     public static void main(String[] args) {
